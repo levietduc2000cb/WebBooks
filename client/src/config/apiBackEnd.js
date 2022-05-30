330b787fc9dc784324c4880f7d3903eaa9484fb3
LeVietDuc2000 <levietduc2000cb@gmail.com>
2022-05-30 22:20:34 +0700

    Test deloy

--- client/src/config/apiBackEnd.js
+++ client/src/config/apiBackEnd.js
@@ -1,8 +1,8 @@
 export const pathAuth =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api/v1/auth"
-    : "https://nameless-journey-21291.herokuapp.com/api/v1/auth";
+    : "https://sleepy-dusk-72204.herokuapp.com/api/v1/auth";
 export const pathProducts =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api/v1/products"
-    : "https://nameless-journey-21291.herokuapp.com/api/v1/products";
+    : "https://sleepy-dusk-72204.herokuapp.com/api/v1/products";
