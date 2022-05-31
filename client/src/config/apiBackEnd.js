09e671ed65fab60b4a3828273ff6f2e8de82dbea
LeVietDuc2000 <levietduc2000cb@gmail.com>
2022-05-31 09:18:46 +0700

    Fix Login Default

--- client/src/config/apiBackEnd.js
+++ client/src/config/apiBackEnd.js
@@ -1,8 +1,10 @@
 export const pathAuth =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api/v1/auth"
-    : "https://sleepy-dusk-72204.herokuapp.com/api/v1/auth";
+    : "https://shrouded-garden-58771.herokuapp.com/api/v1/auth";
 export const pathProducts =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api/v1/products"
-    : "https://sleepy-dusk-72204.herokuapp.com/api/v1/products";
+    : "https://shrouded-garden-58771.herokuapp.com/api/v1/products";
+//http://localhost:5000/api/v1/auth
+//http://localhost:5000/api/v1/products
